7903f4cf5cc25445e95d4c8fb7d5a22af8cde41b
Crokwar <cristomar309@gmail.com>
2025-05-16 20:03:32 -0500

    Add all CRUD routes

--- todolist/server-app/config.py
+++ todolist/server-app/config.py
@@ -2,5 +2,5 @@
 
 class Config:
 
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:3209513635_Cris@localhost/todolist"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:3209513635_Cris@localhost/restaurant_app"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- todolist/server-app/main.py
+++ todolist/server-app/main.py
@@ -1,32 +1,6 @@
-from flask import Flask, render_template
-from flask_cors import CORS
+from app import create_app
 
-app = Flask(__name__)
-CORS(app)
-
-@app.route('/', methods=['GET'])
-def home():
-    return render_template('index.html')
-
-@app.route('/api/users')
-def get_users():
-    return{
-        'users' : [
-            {
-                'id': 1,
-                'name': 'Alice'
-            },
-            {
-                'id': 2,
-                'name': 'Tata'
-            },
-            {
-                'id': 3,
-                'name': 'Cristian'
-            }
-        ]
-    }
-
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
